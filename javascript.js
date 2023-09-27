178e3dde4e49e58a3d0e1762f5a333594bc388a4
hussain.a121145 <140621473+hussainalamin701@users.noreply.github.com>
2023-09-28 00:53:48 +0100

    Added in a moving background and atylized the webpage am now working on the JS side of the project

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-let f_name = document.getElementById("f_name");
+const password = document.getElementById("password");
+const passwordC = document.getElementById("passwordC");
+
+const submit_button = document.getElementById("submit-button");
+
+function checkPassword(){
+    if(passwordC.value !== password.value){
+        alert("Wrong password confirmation");
+
+        passwordC.style.outline = 'red';
+    }else{
+        alert("Successfully signed up");
+    }
+}
+
+submit_button.addEventListener('click', checkPassword)
